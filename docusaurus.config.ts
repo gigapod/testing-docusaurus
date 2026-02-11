e12b56f0ca0dda2599a4a45d5332ef250e277686
Kirk Benell <kirk.benell@sparkfun.com>
2026-02-11 14:26:17 -0700

    fix base url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
     url: 'https://gigapod.dev',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
-    baseUrl: '/',
+    baseUrl: '/testing-docusaurus/',
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
