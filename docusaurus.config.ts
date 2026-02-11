46508cb2d5971370cfcdf15dcffd21b69eda4eec
Kirk Benell <kirk.benell@sparkfun.com>
2026-02-11 13:43:10 -0700

    prep for pages deploy

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,17 +15,18 @@ const config: Config = {
     },
 
     // Set the production url of your site here
-    url: 'https://docs.sparkfun.com/',
+    url: 'https://gigapod.dev',
     // Set the /<baseUrl>/ pathname under which your site is served
     // For GitHub pages deployment, it is often '/<projectName>/'
     baseUrl: '/',
 
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
-    organizationName: 'facebook', // Usually your GitHub org/user name.
-    projectName: 'docusaurus', // Usually your repo name.
+    organizationName: 'gigapod', // Usually your GitHub org/user name.
+    projectName: 'testing-docusaurus', // Usually your repo name.
 
     onBrokenLinks: 'throw',
+    trailingSlash: false,
 
     // Even if you don't use internationalization, you can use this field to set
     // useful metadata like html lang. For example, if your site is Chinese, you
