00abed76651fe3c09ce7fa6e29a515c75b1d293d
Kirk Benell <kirk.benell@sparkfun.com>
2026-02-12 12:25:27 -0700

    add top announce bar for testing

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -59,6 +59,10 @@ const config: Config = {
         colorMode: {
             respectPrefersColorScheme: true,
         },
+        announcementBar: {
+        id: `announcementBar-v1.5.0.beta1`, // Increment on new announcement
+        content: `🎉️ <b><a target="_blank" href="https://github.com/sparkfun/SparkFun_DataLogger/releases/tag/v01.05.00-beta1">DataLogger v1.5.0 Beta</a> is out for testing!</b> 🥳️`,
+      },
         navbar: {
             title: 'DataLogger IoT Documentation',
             logo: {
